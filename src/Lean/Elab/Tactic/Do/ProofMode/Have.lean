c1e0318e99907b5ca28067a30d6ad98e1ac0e70b
volodeyka <vovaglad00@gmail.com>
2026-05-13 20:44:06 +0800

    add new `mhave`

--- src/Lean/Elab/Tactic/Do/ProofMode/Have.lean
+++ src/Lean/Elab/Tactic/Do/ProofMode/Have.lean
@@ -17,7 +17,6 @@ namespace Lean.Elab.Tactic.Do.ProofMode
 open Std.Do SPred.Tactic
 open Lean Elab Tactic Meta
 
-@[builtin_tactic Lean.Parser.Tactic.mdup]
 def elabMDup : Tactic
   | `(tactic| mdup $h:ident => $h₂:ident) => do
     let (mvar, goal) ← ensureMGoal
@@ -37,12 +36,10 @@ def elabMDup : Tactic
     replaceMainGoal [m.mvarId!]
   | _ => throwUnsupportedSyntax
 
-@[builtin_tactic Lean.Parser.Tactic.mhave]
 def elabMHave : Tactic
   | `(tactic| mhave $h $[: $ty?]? := $rhs) => do
     let (mvar, goal) ← ensureMGoal
     mvar.withContext do
-    -- build goal `P ⊢ₛ T` from `P ⊢ₛ H` and residual goal `P ∧ H ⊢ₛ T`
     let P := goal.hyps
     let spred := mkApp (mkConst ``SPred [goal.u]) goal.σs
     let H ← match ty? with
@@ -62,12 +59,10 @@ def elabMHave : Tactic
     replaceMainGoal [m.mvarId!]
   | _ => throwUnsupportedSyntax
 
-@[builtin_tactic Lean.Parser.Tactic.mreplace]
 def elabMReplace : Tactic
   | `(tactic| mreplace $h $[: $ty?]? := $rhs) => do
     let (mvar, goal) ← ensureMGoal
     mvar.withContext do
-    -- build goal `P ⊢ₛ T` from `P ⊢ₛ H` and residual goal `P ∧ H ⊢ₛ T`
     let PH := goal.hyps
     let some res := goal.focusHyp h.raw.getId | throwError m!"Hypothesis {h} not found"
     let P := res.restHyps
@@ -90,3 +85,115 @@ def elabMReplace : Tactic
     mvar.assign prf
     replaceMainGoal [m.mvarId!]
   | _ => throwUnsupportedSyntax
+
+end Lean.Elab.Tactic.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Internal.Do.ProofMode
+
+open Lean Elab Tactic Meta
+open Std.Internal.Tactic.Do
+open Lean.Order
+open Std.Internal.Do.CompleteLattice.Tactic
+
+def elabMDup : Tactic
+  | `(tactic| mdup $h:ident => $h₂:ident) => do
+    let (mvar, goal) ← ensureMGoal
+    mvar.withContext do
+    let some res := goal.focusHyp h.raw.getId | throwError m!"Hypothesis {h} not found"
+    let P := goal.hyps
+    let Q := res.restHyps
+    let H := res.focusHyp
+    let uniq ← mkFreshId
+    let hyp := Hyp.mk h₂.raw.getId uniq H.consumeMData
+    addHypInfo h goal.u goal.l goal.cl hyp (isBinder := true)
+    let H' := hyp.toExpr
+    let T := goal.target
+    let newGoal := { goal with hyps := mkAnd! goal.uType goal.l goal.cl P H' }
+    let m ← mkFreshExprSyntheticOpaqueMVar newGoal.toExpr
+    -- Have.dup {P Q H T : l} (hfoc : P = Q ⊓ H) (hgoal : P ⊓ H ⊑ T) : P ⊑ T
+    -- args: l, cl, P, Q, H, T, hfoc, hgoal (8 args)
+    mvar.assign (mkApp8 (mkConst ``Have.dup [goal.uType])
+      goal.l goal.cl P Q H T res.proof m)
+    replaceMainGoal [m.mvarId!]
+  | _ => throwUnsupportedSyntax
+
+def elabMHave : Tactic
+  | `(tactic| mhave $h $[: $ty?]? := $rhs) => do
+    let (mvar, goal) ← ensureMGoal
+    mvar.withContext do
+    let P := goal.hyps
+    let H ← match ty? with
+      | some ty => elabTerm ty goal.l
+      | _       => mkFreshExprMVar goal.l
+    let uniq ← mkFreshId
+    let hyp := Hyp.mk h.raw.getId uniq H
+    addHypInfo h goal.u goal.l goal.cl hyp (isBinder := true)
+    let H := hyp.toExpr
+    let T := goal.target
+    let (PH, hand) := mkAnd goal.uType goal.l goal.cl P H
+    let haveGoal := { goal with target := H }
+    let hhave ← elabTermEnsuringType rhs haveGoal.toExpr
+    let newGoal := { goal with hyps := PH }
+    let m ← mkFreshExprSyntheticOpaqueMVar newGoal.toExpr
+    -- Have.have {P H PH T : l} (hand : P ⊓ H = PH) (hhave : P ⊑ H) (hgoal : PH ⊑ T) : P ⊑ T
+    -- args: l, cl, P, H, PH, T, hand, hhave, hgoal (9 args)
+    mvar.assign (mkApp9 (mkConst ``Have.have [goal.uType])
+      goal.l goal.cl P H PH T hand hhave m)
+    replaceMainGoal [m.mvarId!]
+  | _ => throwUnsupportedSyntax
+
+def elabMReplace : Tactic
+  | `(tactic| mreplace $h $[: $ty?]? := $rhs) => do
+    let (mvar, goal) ← ensureMGoal
+    mvar.withContext do
+    let PH := goal.hyps
+    let some res := goal.focusHyp h.raw.getId | throwError m!"Hypothesis {h} not found"
+    let P := res.restHyps
+    let H := res.focusHyp
+    let H' ← match ty? with
+      | some ty => elabTerm ty goal.l
+      | _       => mkFreshExprMVar goal.l
+    let uniq ← mkFreshId
+    let hyp := Hyp.mk h.raw.getId uniq H'
+    addHypInfo h goal.u goal.l goal.cl hyp (isBinder := true)
+    let H' := hyp.toExpr
+    let haveGoal := { goal with target := H' }
+    let hhave ← elabTermEnsuringType rhs haveGoal.toExpr
+    let T := goal.target
+    let (PH', hand) := mkAnd goal.uType goal.l goal.cl P H'
+    let newGoal := { goal with hyps := PH' }
+    let m ← mkFreshExprSyntheticOpaqueMVar newGoal.toExpr
+    -- Have.replace {P H H' PH PH' T : l} (hfoc : PH = P ⊓ H) (hand : P ⊓ H' = PH')
+    --   (hhave : PH ⊑ H') (hgoal : PH' ⊑ T) : PH ⊑ T
+    -- args: l, cl, P, H, H', PH, PH', T, hfoc, hand, hhave, hgoal (12 args)
+    let prf := mkAppN (mkConst ``Have.replace [goal.uType])
+      #[goal.l, goal.cl, P, H, H', PH, PH', T, res.proof, hand, hhave, m]
+    mvar.assign prf
+    replaceMainGoal [m.mvarId!]
+  | _ => throwUnsupportedSyntax
+
+end Lean.Elab.Tactic.Internal.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Do.ProofMode
+open Std.Do SPred.Tactic
+open Lean Elab Tactic Meta
+
+@[builtin_tactic Lean.Parser.Tactic.mdup]
+def elabMDupOpt : Tactic := fun stx => do
+  if new_proof_mode.get (← getOptions) then
+    return ← Lean.Elab.Tactic.Internal.Do.ProofMode.elabMDup stx
+  elabMDup stx
+
+@[builtin_tactic Lean.Parser.Tactic.mhave]
+def elabMHaveOpt : Tactic := fun stx => do
+  if new_proof_mode.get (← getOptions) then
+    return ← Lean.Elab.Tactic.Internal.Do.ProofMode.elabMHave stx
+  elabMHave stx
+
+@[builtin_tactic Lean.Parser.Tactic.mreplace]
+def elabMReplaceOpt : Tactic := fun stx => do
+  if new_proof_mode.get (← getOptions) then
+    return ← Lean.Elab.Tactic.Internal.Do.ProofMode.elabMReplace stx
+  elabMReplace stx
+
+end Lean.Elab.Tactic.Do.ProofMode

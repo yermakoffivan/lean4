b097f74c0e393f5d157fc25c550e9dbfd7bb17fc
volodeyka <vovaglad00@gmail.com>
2026-05-13 20:41:12 +0800

    add focusing for CompleteLattice-based proof mode

--- src/Lean/Elab/Tactic/Do/ProofMode/Focus.lean
+++ src/Lean/Elab/Tactic/Do/ProofMode/Focus.lean
@@ -67,3 +67,80 @@ def MGoal.focusHypWithInfo (goal : MGoal) (name : Ident) : MetaM FocusResult := 
   let some hyp := parseHyp? res.focusHyp | throwError "impossible; res.focusHyp not a hypothesis"
   addHypInfo name goal.σs hyp
   pure res
+
+end Lean.Elab.Tactic.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Internal.Do.ProofMode
+
+open Lean Elab.Tactic Meta
+open Std.Internal.Tactic.Do
+open Lean.Order
+open Std.Internal.Do.CompleteLattice.Tactic
+
+/-- The result of focusing the context of a goal `goal : MGoal` on a particular hypothesis.
+The focused hypothesis is returned as `focusHyp : Expr`, along with the residual `restHyps : Expr`
+and a `proof : Expr` for the property `goal.hyps = restHyps ⊓ focusHyp`. -/
+structure FocusResult where
+  focusHyp : Expr
+  restHyps : Expr
+  proof : Expr
+  deriving Inhabited
+
+partial def focusHyp (u : Level) (l cl : Expr) (e : Expr) (name : Name) : Option FocusResult := do
+  if let some hyp := parseHyp? e then
+    if hyp.name = name then
+      -- Focus.this {P : l} : P = ⊤ ⊓ P  -- args: l, cl, P
+      return ⟨e, emptyHyp u l cl, mkApp3 (mkConst ``Focus.this [u]) l cl e⟩
+    else
+      none
+  else if let some (u', _l', _cl', lhs, rhs) := parseAnd? e then
+    try
+      -- NB: Need to prefer rhs over lhs, like the goal view.
+      let ⟨focus, rhs', h₁⟩ ← focusHyp u' l cl rhs name
+      let ⟨C, h₂⟩ := mkAnd u' l cl lhs rhs'
+      -- Focus.right {P Q Q' C R} h₁ h₂ : P ⊓ Q = C ⊓ R  -- args: l cl P Q Q' C R h₁ h₂
+      return ⟨focus, C,
+        mkApp9 (mkConst ``Focus.right [u]) l cl lhs rhs rhs' C focus h₁ h₂⟩
+    catch _ =>
+      let ⟨focus, lhs', h₁⟩ ← focusHyp u' l cl lhs name
+      let ⟨C, h₂⟩ := mkAnd u' l cl lhs' rhs
+      -- Focus.left {P P' Q C R} h₁ h₂ : P ⊓ Q = C ⊓ R  -- args: l cl P P' Q C R h₁ h₂
+      return ⟨focus, C,
+        mkApp9 (mkConst ``Focus.left [u]) l cl lhs lhs' rhs C focus h₁ h₂⟩
+  else if let some _ := parseEmptyHyp? e then
+    none
+  else
+    panic! s!"focusHyp: hypothesis without proper metadata: {e}"
+
+def MGoal.focusHyp (goal : MGoal) (name : Name) : Option FocusResult :=
+  Lean.Elab.Tactic.Internal.Do.ProofMode.focusHyp goal.uType goal.l goal.cl goal.hyps name
+
+def FocusResult.refl (u : Level) (l cl : Expr) (restHyps : Expr) (focusHyp : Expr) : FocusResult :=
+  let result := mkAnd! u l cl restHyps focusHyp
+  -- proof : (restHyps ⊓ focusHyp) = (restHyps ⊓ focusHyp), via Eq.refl
+  let proof := mkApp2 (mkConst ``Eq.refl [.succ u]) l result
+  { restHyps, focusHyp, proof }
+
+def FocusResult.restGoal (res : FocusResult) (goal : MGoal) : MGoal :=
+  { goal with hyps := res.restHyps }
+
+def FocusResult.recombineGoal (res : FocusResult) (goal : MGoal) : MGoal :=
+  { goal with hyps := mkAnd! goal.uType goal.l goal.cl res.restHyps res.focusHyp }
+
+/-- Turn a proof for `(res.recombineGoal goal).toExpr` into one for `goal.toExpr`. -/
+def FocusResult.rewriteHyps (res : FocusResult) (goal : MGoal) : Expr → Expr :=
+  -- Focus.rewrite_hyps {P Q R} (hrw) (hgoal) : P ⊑ R
+  -- args: l cl P Q R hrw hgoal (7 args). We partially apply with 6 (leaving hgoal).
+  let recombined := mkAnd! goal.uType goal.l goal.cl res.restHyps res.focusHyp
+  mkApp7 (mkConst ``Focus.rewrite_hyps [goal.uType])
+    goal.l goal.cl goal.hyps recombined goal.target res.proof
+
+def MGoal.focusHypWithInfo (goal : MGoal) (name : Ident) : MetaM FocusResult := do
+  let some res := goal.focusHyp name.getId | throwError "unknown hypothesis `{name}`"
+  let some hyp := parseHyp? res.focusHyp | throwError "impossible; res.focusHyp not a hypothesis"
+  addHypInfo name goal.u goal.l goal.cl hyp
+  pure res
+
+end Lean.Elab.Tactic.Internal.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Do.ProofMode

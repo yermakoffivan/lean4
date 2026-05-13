e957c01947e0dc35e84f617b838cc13e3891bba3
volodeyka <vovaglad00@gmail.com>
2026-05-13 20:34:26 +0800

    Implement `elabMClearOpt` tactic to toggle between legacy and new proof modes, enhancing proof-mode functionality with `CompleteLattice` support.

--- src/Lean/Elab/Tactic/Do/ProofMode/Clear.lean
+++ src/Lean/Elab/Tactic/Do/ProofMode/Clear.lean
@@ -15,7 +15,6 @@ namespace Lean.Elab.Tactic.Do.ProofMode
 open Std.Do SPred.Tactic
 open Lean Elab Tactic Meta
 
-@[builtin_tactic Lean.Parser.Tactic.mclear]
 def elabMClear : Tactic
   | `(tactic| mclear $hyp:ident) => do
     let mvar ← getMainGoal
@@ -29,3 +28,42 @@ def elabMClear : Tactic
       res.restHyps res.focusHyp goal.target res.proof m)
     replaceMainGoal [m.mvarId!]
   | _ => throwUnsupportedSyntax
+
+end Lean.Elab.Tactic.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Internal.Do.ProofMode
+
+open Lean Elab.Tactic Meta
+open Std.Internal.Tactic.Do
+open Lean.Order
+open Std.Internal.Do.CompleteLattice.Tactic
+
+def elabMClear : Tactic
+  | `(tactic| mclear $hyp:ident) => do
+    let mvar ← getMainGoal
+    mvar.withContext do
+    let g ← instantiateMVars <| ← mvar.getType
+    let some goal := parseMGoal? g | throwError "not in proof mode"
+    let res ← goal.focusHypWithInfo hyp
+    let m ← mkFreshExprSyntheticOpaqueMVar (res.restGoal goal).toExpr
+    -- Clear.clear {P P' A Q} (hfocus : P = P' ⊓ A) (h : P' ⊑ Q) : P ⊑ Q
+    -- args: l cl P P' A Q hfocus h (8 args)
+    mvar.assign (mkApp8 (mkConst ``Clear.clear [goal.uType]) goal.l goal.cl goal.hyps
+      res.restHyps res.focusHyp goal.target res.proof m)
+    replaceMainGoal [m.mvarId!]
+  | _ => throwUnsupportedSyntax
+
+end Lean.Elab.Tactic.Internal.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Do.ProofMode
+open Std.Do SPred.Tactic
+open Lean Elab Tactic Meta
+
+@[builtin_tactic Lean.Parser.Tactic.mclear]
+def elabMClearOpt : Tactic := fun stx => do
+  if new_proof_mode.get (← getOptions) then
+    Lean.Elab.Tactic.Internal.Do.ProofMode.elabMClear stx
+  else
+    elabMClear stx
+
+end Lean.Elab.Tactic.Do.ProofMode

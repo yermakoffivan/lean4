6c8128aa0d287fbc9bff5374a7b7016c5f9ed7c8
volodeyka <vovaglad00@gmail.com>
2026-05-13 20:35:02 +0800

    Enhance `elabMConstructor` functionality by introducing `elabMConstructorOpt` to toggle between legacy and new proof modes, improving support for `Lean.Order` constructs in proof mode.

--- src/Lean/Elab/Tactic/Do/ProofMode/Constructor.lean
+++ src/Lean/Elab/Tactic/Do/ProofMode/Constructor.lean
@@ -26,9 +26,50 @@ def mConstructorCore (mvar : MVarId) : MetaM (MVarId × MVarId) := do
   mvar.assign (mkApp6 (mkConst ``SPred.and_intro [goal.u]) σs goal.hyps L R leftGoal rightGoal)
   return (leftGoal.mvarId!, rightGoal.mvarId!)
 
-@[builtin_tactic Lean.Parser.Tactic.mconstructor]
 def elabMConstructor : Tactic | _ => do
   let mvar ← getMainGoal
   mvar.withContext do
   let (leftGoal, rightGoal) ← mConstructorCore mvar
   replaceMainGoal [leftGoal, rightGoal]
+
+end Lean.Elab.Tactic.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Internal.Do.ProofMode
+
+open Lean Elab.Tactic Meta
+open Std.Internal.Tactic.Do
+open Lean.Order
+
+def mConstructorCore (mvar : MVarId) : MetaM (MVarId × MVarId) := do
+  let g ← instantiateMVars <| ← mvar.getType
+  let some goal := parseMGoal? g | throwError "not in proof mode"
+
+  -- target should be `Lean.Order.meet l cl L R`
+  let mkApp4 (.const ``Lean.Order.meet _) _l _cl L R := goal.target
+    | throwError "target is not Lean.Order.meet"
+
+  let leftGoal ← mkFreshExprSyntheticOpaqueMVar {goal with target := L}.toExpr
+  let rightGoal ← mkFreshExprSyntheticOpaqueMVar {goal with target := R}.toExpr
+  mvar.assign (mkApp7 (mkConst ``Lean.Order.le_meet [goal.uType]) goal.l goal.cl
+    goal.hyps L R leftGoal rightGoal)
+  return (leftGoal.mvarId!, rightGoal.mvarId!)
+
+def elabMConstructor : Tactic | _ => do
+  let mvar ← getMainGoal
+  mvar.withContext do
+  let (leftGoal, rightGoal) ← mConstructorCore mvar
+  replaceMainGoal [leftGoal, rightGoal]
+
+end Lean.Elab.Tactic.Internal.Do.ProofMode
+
+namespace Lean.Elab.Tactic.Do.ProofMode
+open Std.Do
+open Lean Elab Tactic Meta
+
+@[builtin_tactic Lean.Parser.Tactic.mconstructor]
+def elabMConstructorOpt : Tactic := fun stx => do
+  if new_proof_mode.get (← getOptions) then
+    return ← Lean.Elab.Tactic.Internal.Do.ProofMode.elabMConstructor stx
+  elabMConstructor stx
+
+end Lean.Elab.Tactic.Do.ProofMode

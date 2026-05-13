8da68789642a235e1e68e8482cd327878782ead9
volodeyka <vovaglad00@gmail.com>
2026-05-13 20:38:15 +0800

    add technical ProofMode definitions

--- src/Std/Tactic/Do/ProofMode.lean
+++ src/Std/Tactic/Do/ProofMode.lean
@@ -7,6 +7,7 @@ module
 
 prelude
 public import Std.Do.SPred.SPred
+public import Init.Internal.Order
 
 @[expose] public section
 
@@ -18,6 +19,7 @@ open Std.Do
 syntax mgoalHyp := ident " : " term
 
 syntax mgoalStx := ppDedent(ppLine mgoalHyp)* ppDedent(ppLine "⊢ₛ " term)
+syntax mgoalOrderStx := ppDedent(ppLine mgoalHyp)* ppDedent(ppLine "⊑ " term)
 
 /-- This is the same as `SPred.entails`.
 This constant is used to detect `SPred` proof mode goals. -/
@@ -28,3 +30,16 @@ to have type `A : PROP` even though `PROP` is not a type. -/
 def MGoalHypMarker {σs : List (Type u)} (_A : SPred σs) : Prop := True
 
 end Std.Tactic.Do
+
+namespace Std.Internal.Tactic.Do
+
+/-- This is the same as `PartialOrder.rel` for the partial order induced by `CompleteLattice`.
+This constant is used to detect `Assertion` proof mode goals. -/
+abbrev MGoalEntails {l : Sort u} [self : Lean.Order.CompleteLattice l] :=
+  @Lean.Order.PartialOrder.rel l self.toPartialOrder
+
+/-- This is only used for delaboration purposes, so that we can render context variables that appear
+to have type `A : PROP` even though `PROP` is not a type. -/
+def MGoalHypMarker {l : Sort u} [Lean.Order.CompleteLattice l] (_A : l) : Prop := True
+
+end Std.Internal.Tactic.Do

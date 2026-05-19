f64af6aad53d4c6798eaadeca3b65f37281d9467
Kyle Miller <kmill31415@gmail.com>
2026-05-18 23:03:49 -0700

    refactor: use builtin command elaborators for configuration evaluation
    This PR makes the command elaborators for configuration evaluation metaprogramming be builtins, to avoid bootstrapping issues in core Lean, due to the interpreter being used to run the metaprograms before new builtin initializers are run, among other ABI issues. (This is part 1, which will have a part 2 after a stage0 update.)

--- src/Lean/Elab/ConfigEval/MetaInstances.lean
+++ src/Lean/Elab/ConfigEval/MetaInstances.lean
@@ -7,6 +7,7 @@ module
 
 prelude
 public import Lean.Elab.ConfigEval.Commands
+public import Lean.Elab.ConfigEval.Builtins -- TODO(kmill): remove after stage0 update
 public import Lean.Elab.ConfigEval.Instances
 public import Lean.Elab.DeprecatedSyntax  -- shake: skip (workaround for command elaborators failing to interpret `deprecatedSyntaxExt`, to be fixed #13108)
 

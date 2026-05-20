cc46c7d2cec2714a74eb4b15a8cfb64d92a36436
Sofia Rodrigues <sofia@algebraic.dev>
2026-05-20 20:01:14 +0100

    fix: change to derivation again to test pkgs.callPackage

--- flake.nix
+++ flake.nix
@@ -35,24 +35,17 @@
           # more convenient `ctest` output
           CTEST_OUTPUT_ON_FAILURE = 1;
         } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux (let
-          # Build OpenSSL 3 statically using pkgsDist's old-glibc stdenv,
+          # Build OpenSSL using pkgsDist's old-glibc stdenv,
           # so the resulting static libs don't require newer glibc symbols.
-          opensslForDist = pkgsDist.stdenv.mkDerivation {
-            name = "openssl-static-3.6.0";
+          opensslForDist = (pkgsDist.callPackage (pkgs.path + "/pkgs/development/libraries/openssl") {}).overrideAttrs (attrs: {
             src = pkgs.fetchFromGitHub {
               owner = "openssl";
               repo = "openssl";
               rev = "openssl-3.6.0";
               hash = "sha256-EJnbK9ZMdN2ztTTQtb7VsEQvvbMYnY5HJ2LMJlw5FRg=";
             };
-            nativeBuildInputs = [ pkgsDist.perl ];
-            configurePhase = ''
-              patchShebangs .
-              ./config --prefix=$out no-shared no-tests
-            '';
-            buildPhase = "make -j$NIX_BUILD_CORES";
-            installPhase = "make install_sw";
-          };
+            configureFlags = (attrs.configureFlags or []) ++ [ "no-shared" "no-tests" ];
+          });
         in {
           GMP = (pkgsDist.gmp.override { withStatic = true; }).overrideAttrs (attrs:
             pkgs.lib.optionalAttrs (pkgs.stdenv.system == "aarch64-linux") {

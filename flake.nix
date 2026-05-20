7ca3cac30fcf8dc7f6e9de5f404f20009567d932
Sofia Rodrigues <sofia@algebraic.dev>
2026-05-20 22:09:52 +0100

    fix: override with pkgs and using stdenv of pkgsDist

--- flake.nix
+++ flake.nix
@@ -37,22 +37,12 @@
         } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux (let
           # Build OpenSSL 3 statically using pkgsDist's old-glibc stdenv,
           # so the resulting static libs don't require newer glibc symbols.
-          opensslForDist = pkgsDist.stdenv.mkDerivation {
-            name = "openssl-static-3.6.0";
-            src = pkgs.fetchFromGitHub {
-              owner = "openssl";
-              repo = "openssl";
-              rev = "openssl-3.6.0";
-              hash = "sha256-EJnbK9ZMdN2ztTTQtb7VsEQvvbMYnY5HJ2LMJlw5FRg=";
-            };
-            nativeBuildInputs = [ pkgsDist.perl ];
-            configurePhase = ''
-              patchShebangs .
-              ./config --prefix=$out no-shared no-tests
-            '';
-            buildPhase = "make -j$NIX_BUILD_CORES";
-            installPhase = "make install_sw";
-          };
+          opensslForDist = (pkgs.openssl.override {
+            static = true;
+          }).overrideAttrs (old: {
+            stdenv = pkgsDist.stdenv;
+            nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgsDist.perl ];
+          });
         in {
           GMP = (pkgsDist.gmp.override { withStatic = true; }).overrideAttrs (attrs:
             pkgs.lib.optionalAttrs (pkgs.stdenv.system == "aarch64-linux") {

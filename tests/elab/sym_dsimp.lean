f4d33544a011c918b04e44b2f15180f1ae002e47
Leonardo de Moura <leomoura@amazon.com>
2026-05-26 20:34:55 +0200

    test:

--- tests/elab/sym_dsimp.lean
+++ tests/elab/sym_dsimp.lean
@@ -26,3 +26,36 @@ example (h : 10 + a = b) : let x := 10; let y := a; x + y = b := by
     dsimp [*]
     show_goals
     exact h
+
+register_sym_dsimp myDSimp where
+  pre  := beta >> match >> zeta >> zeta_delta
+  post := none
+
+/--
+trace: case grind
+a b : Nat
+h : 10 + a = b
+⊢ 10 + a = b
+-/
+#guard_msgs in
+example (h : 10 + a = b) : let x := 10; let y := a; x + y = b := by
+  sym =>
+    fail_if_success dsimp
+    dsimp myDSimp
+    show_goals
+    exact h
+
+/--
+trace: case grind
+a b : Nat
+β✝ : Type u_1
+c : β✝
+h : 10 + a = b
+⊢ 10 + a = (b, c).fst
+-/
+#guard_msgs in
+example (h : 10 + a = b) : let x := 10; let y := a; x + y = (b, c).1 := by
+  sym =>
+    dsimp myDSimp -- projections are not reduced
+    show_goals
+    exact h

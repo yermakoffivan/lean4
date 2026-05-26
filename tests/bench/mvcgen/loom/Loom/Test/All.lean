fe62617d2bfced204d9b428205b506311137e0ac
volodeyka <vovaglad00@gmail.com>
2026-05-26 13:59:55 +0800

    add tests for simp-spec

--- tests/bench/mvcgen/loom/Loom/Test/All.lean
+++ tests/bench/mvcgen/loom/Loom/Test/All.lean
@@ -1,9 +1,11 @@
 import Loom.Test.AddSubCancel
+import Loom.Test.AddSubCancelSimp
 import Loom.Test.AddSubCancelDeep
 import Loom.Test.AssertGadgetStep
 import Loom.Test.ConcreteEPostTwoPreds
 import Loom.Test.ConcretePostEPost
 import Loom.Test.DiteSplit
+import Loom.Test.EqualitySpecs
 import Loom.Test.GetThrowSet
 import Loom.Test.MatchSplit
 import Loom.Test.MatchSplitTest
@@ -14,6 +16,7 @@ set_option maxHeartbeats 10000000
 open Lean Parser Meta Elab Tactic Sym Loom Std.Internal.Do
 
 #eval AddSubCancel.runTests [10]
+#eval AddSubCancelSimp.runTests [10]
 #eval AddSubCancelDeep.runTests [10]
 #eval AssertGadgetStep.runTests [10]
 #eval ConcreteEPostTwoPreds.runTests [10]

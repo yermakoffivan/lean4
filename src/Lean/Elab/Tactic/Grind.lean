9bc18ac2402bd094b4115054ef093bc31d0d1253
Leonardo de Moura <leomoura@amazon.com>
2026-05-26 17:47:11 +0200

    feat: elaborate `register_sym_dsimp`

--- src/Lean/Elab/Tactic/Grind.lean
+++ src/Lean/Elab/Tactic/Grind.lean
@@ -19,3 +19,5 @@ public import Lean.Elab.Tactic.Grind.Sym
 public import Lean.Elab.Tactic.Grind.SimprocDSL
 public import Lean.Elab.Tactic.Grind.SimprocDSLBuiltin
 public import Lean.Elab.Tactic.Grind.RegisterSymSimp
+public import Lean.Elab.Tactic.Grind.DSimprocDSL
+public import Lean.Elab.Tactic.Grind.RegisterSymDSimp

291fbe7482ad2fb6c400f8de9b1c640d195f9641
Henrik Böving <hargonix@gmail.com>
2026-05-27 16:44:31 +0000

    silence linters

--- tests/elab/grind_lint_bitvec.lean
+++ tests/elab/grind_lint_bitvec.lean
@@ -6,4 +6,14 @@ import Lean.Elab.Tactic.Grind.LintExceptions
 /-! Check BitVec namespace: -/
 
 #guard_msgs in
-#grind_lint check (min := 20) in BitVec
+#grind_lint inspect (min := 21) BitVec.msb_extractLsb
+
+#guard_msgs in
+#grind_lint inspect (min := 21) BitVec.msb_signExtend
+
+#guard_msgs in
+#grind_lint inspect (min := 21) BitVec.toInt_shiftLeftZeroExtend
+
+#guard_msgs in
+#grind_lint check  (min := 20) in BitVec
+

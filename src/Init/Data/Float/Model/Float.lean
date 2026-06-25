fad786dc2d53b00b059940c0e780d0146d5c15ab
Julia Markus Himmel <2065352+TwoFX@users.noreply.github.com>
2026-06-25 13:56:56 +0000

    Whitespace

--- src/Init/Data/Float/Model/Float.lean
+++ src/Init/Data/Float/Model/Float.lean
@@ -37,6 +37,7 @@ structure Float.Model where
   /-- The underlying bit pattern is valid according to the IEEE `binary64` format. -/
   valid : Float.Model.Format.binary64.Valid toBits.toBitVec
 deriving DecidableEq
+
 namespace Float.Model
 
 /-- Unpack a `Float.Model` into the corresponding `UnpackedFloat`. -/

a22643fc33306c94559dc385771316bee91a794d
Leonardo de Moura <leomoura@amazon.com>
2026-07-04 18:34:54 +0000

    chore: remove leftover

--- tests/elab/sym_apply_univ_issues.lean
+++ tests/elab/sym_apply_univ_issues.lean
@@ -4,7 +4,6 @@ opaque rel {α : Type u} : α → α → Prop
 
 theorem foo {σ : Type u} {α : Type v} {f g : σ → α} : rel f g := sorry
 
-set_option trace.Meta.debug true in
 theorem bar {Pred : Type u} (f : Nat → Pred) :
   rel f f := by
   sym => apply foo -- fails
